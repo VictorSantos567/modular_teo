0882839d7d58b790f7ddf7a19287e67da6e82645
Leo <l1n3rd4@localhost.localdomain>
2020-04-14 17:56:50 -0300

    Adicionanto propriedades do  ingresso

--- src/ingresso/Ingressos.java
+++ src/ingresso/Ingressos.java
@@ -1,5 +1,36 @@
 package ingresso;
 
-public class Ingressos{
+import java.time.LocalDateTime;
 
+public class Ingressos{
+	private String nome;
+	private double valor;
+	private LocalDateTime data;
+	
+	public Ingressos(String nome, double valor, int ano, int mes, int dia, int horas, int minutos) {
+		setNome(nome);
+		setValor(valor);
+		data = LocalDateTime.of(ano, mes, dia, horas, minutos);
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public LocalDateTime getData() {
+		return data;
+	}
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+	
 }
